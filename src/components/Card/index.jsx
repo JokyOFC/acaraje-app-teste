f4d6792311cd495c8a51b9355ce6e1e21ac67c8f
JokyOFC <personal_joky@outlook.com>
2022-10-26 13:17:31 -0300

    updating

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,19 +1,25 @@
-import { StyleSheet, View } from "react-native"
+import { StyleSheet, TouchableOpacity, View } from "react-native"
 
 
 export const Card = ({ children }) => {
-    return(<View style={styles.container}>
-        {children}
-    </View>)
+    return(
+    <TouchableOpacity style={{ width: "100%", alignItems: "center" }}>
+        <View style={styles.container}>
+            {children}
+        </View>
+    </TouchableOpacity>
+    )
 }
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: "#D9D9D9",
         borderRadius: 25,
-        height: "90%",
-        width: "90%",
+        minHeight: 200,
+        padding:25,
+        width: "85%",
         shadowColor: "#D9D9D9",
+        margin: 10,
         shadowRadius: 25,
         shadowOffset: 5,
         shadowOpacity: 2,
--- src/pages/Pedidos/index.jsx
+++ src/pages/Pedidos/index.jsx
@@ -3,18 +3,53 @@ import { SafeAreaView, ScrollView, StyleSheet, View, Text } from "react-native"
 
 import { Card } from "../../components/Card"
 
+import orders from "../../api/json/orders.json"
+// import { RadioButton } from "react-native-paper"
+
+import RadioForm, { RadioButton, RadioButtonInput, RadioButtonLabel } from "react-native-simple-radio-button"
+
 export const Pedidos = () => {
-    return(
-    <SafeAreaView style={{  flex: 1, height: 500, width: 500}}>
-        <View style={{ }}>
-            <ScrollView style={{ maxHeight: 100, maxWidth: 500, marginTop: 100 }}>
-                <Card><Text>1212</Text></Card>
-            </ScrollView>
-        </View>
-    </SafeAreaView>
+
+    const radio_props = [
+        {label: 'Todos', value: 0},
+        {label: 'Ordenar Por X', value: 1},
+        {label: 'Ordenar Por Y', value: 2},
+    ]
+
+    return (
+        <SafeAreaView style={{ flex: 1, height: 500 }}>
+            <View>
+                <View style={styles.top}>
+                    <Text style={{ fontSize: 25 }}>Pedidos</Text>
+                    <View style={{ padding: 10, paddingBottom: 2 }}>
+                        <RadioForm formHorizontal={true} 
+                            radio_props={radio_props}
+                            initial={0}
+                        />
+                    </View>
+                </View>
+                <ScrollView style={{ maxHeight: "87%", maxWidth: 450 }}>
+                    <View style={{ alignItems: "center" }}>
+                        {
+                            orders.map((order) => (
+                                <Card>
+                                    <Text>
+                                        {order.cliente}
+                                    </Text>
+                                </Card>
+                            ))
+                        }
+                    </View>
+                </ScrollView>
+            </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-
+    top: {
+        padding: 35,
+        paddingLeft: 15,
+        paddingBottom: 0
+    }
 })
